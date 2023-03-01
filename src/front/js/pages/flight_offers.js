5044a56f7b07e07d4031835aa901733b7a63f64a
Dali <dalirod90@gmail.com>
2023-03-01 21:58:11 +0000

    probando

--- src/front/js/pages/flight_offers.js
+++ src/front/js/pages/flight_offers.js
@@ -9,13 +9,17 @@ export const Flight_offers = () => {
   const [retorno, setRetorno] = useState("");
   const [adultos, setAdultos] = useState("");
   const [niños, setNiños] = useState("");
-  
 
-  
+  const { store, actions } = useContext(Context);
+  const flight_offers = (event) => {
+    event.priventDefault();
+    actions.flight_offers(origen, destino, salida, retorno, adultos, niños);
+  };
+
   return (
     <>
-      <h1>Elige tu vuelo</h1>
-      <div className="continer-sm">
+      <h1 className="my-5 d-flex justify-content-center">Elige tu vuelo</h1>
+      <form onSubmit={flight_offers}>
         <div className="container-sm border border-dark border border-2">
           <h3>Locacion</h3>
           <div className="row">
@@ -74,133 +78,146 @@ export const Flight_offers = () => {
               </div>
             </div>
           </div>
-        </div>
 
-        {/* <!--  --> */}
-        <div className="container-sm d-flex align-content-start">
-          <div className="row">
+          {/* <!--  --> */}
+          <div className="container d-flex justify-content-center card-vuelos">
+            <div className="d-flex justify-content-center ">
+              <div className="row">
+                <div className="mb-2 col">
+                  <div className="h-100 card">
+                    <div className="card-body">
+                      <h5 className="card-title">Fechas</h5>
+                      <div className="mb-2">
+                        <label
+                          id="flight-type-label"
+                          for="flight-type-select"
+                          className="form-label"
+                        >
+                          Flight
+                        </label>
+                        <select
+                          id="flight-type-select"
+                          className="form-select"
+                          aria-describedby="flight-type-label"
+                        >
+                          <option value="one-way">Solo Ida</option>
+                          <option value="round-trip">Ida y Vuelta</option>
+                        </select>
+                      </div>
+                      <div id="departure-date" className="mb-2">
+                        <div id="departure-date" className="mb-2">
+                          <label
+                            id="departure-date-label"
+                            for="departure-date-input"
+                            className="form-label"
+                          >
+                            Salida
+                          </label>
+                          <div className="input-group">
+                            <span className="input-group-text">
+                              <i className="bi-calendar"></i>
+                            </span>
+                            <input
+                              type="date"
+                              className="form-control"
+                              id="departure-date-input"
+                              aria-describedby="departure-date-label"
+                              onChange={(event) =>
+                                setSalida(event.target.value)
+                              }
+                              value={salida}
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <div id="return-date" className="mb-2">
+                        <div id="return-date" className="mb-2">
+                          <label
+                            id="return-date-label"
+                            for="return-date-input"
+                            className="form-label"
+                          >
+                            Retorno
+                          </label>
+                          <div className="input-group">
+                            <span className="input-group-text">
+                              <i className="bi-calendar-fill"></i>{" "}
+                            </span>
+                            <input
+                              type="date"
+                              className="form-control"
+                              id="return-date-input"
+                              aria-describedby="return-date-label"
+                              onChange={(event) =>
+                                setRetorno(event.target.value)
+                              }
+                              value={retorno}
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  {/* <!-- ... --> */}
+                </div>
+              </div>
+            </div>
             <div className="mb-2 col">
               <div className="h-100 card">
                 <div className="card-body">
-                  <h5 className="card-title">Dates</h5>
-                  <div className="mb-2">
-                    <label
-                      id="flight-type-label"
-                      for="flight-type-select"
-                      className="form-label"
-                    >
-                      Flight
+                  <h5 className="card-title">Detalles</h5>
+                  <div className="input-group">
+                    <label for="adults-input" className="input-group-text">
+                      Adultos
                     </label>
-                    <select
-                      id="flight-type-select"
-                      className="form-select"
-                      aria-describedby="flight-type-label"
-                    >
-                      <option value="one-way">Solo Ida</option>
-                      <option value="round-trip">Ida y Vuelta</option>
-                    </select>
+                    <input
+                      type="number"
+                      min="0"
+                      className="form-control"
+                      id="adults-input"
+                      aria-describedby="adults-label"
+                      onChange={(event) => setAdultos(event.target.value)}
+                      value={adultos}
+                    />
                   </div>
-                  <div id="departure-date" className="mb-2">
-                    <div id="departure-date" className="mb-2">
-                      <label
-                        id="departure-date-label"
-                        for="departure-date-input"
-                        className="form-label"
-                      >
-                        Salida
-                      </label>
-                      <div className="input-group">
-                        <span className="input-group-text">
-                          <i className="bi-calendar"></i>
-                        </span>
-                        <input
-                          type="date"
-                          className="form-control"
-                          id="departure-date-input"
-                          aria-describedby="departure-date-label"
-                          onChange={(event) => setSalida(event.target.value)}
-                          value={salida}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div id="return-date" className="mb-2">
-                    <div id="return-date" className="mb-2">
-                      <label
-                        id="return-date-label"
-                        for="return-date-input"
-                        className="form-label"
-                      >
-                        Retorno
-                      </label>
-                      <div className="input-group">
-                        <span className="input-group-text">
-                          <i className="bi-calendar-fill"></i>{" "}
-                        </span>
-                        <input
-                          type="date"
-                          className="form-control"
-                          id="return-date-input"
-                          aria-describedby="return-date-label"
-                          onChange={(event) => setRetorno(event.target.value)}
-                          value={retorno}
-                        />
-                      </div>
-                    </div>
-                  </div>
+                  <span id="adults-label" className="form-text">
+                    12 years old and older
+                  </span>
                 </div>
-                {/* <!-- ... --> */}
-              </div>
-            </div>
-          </div>
-          <div className="mb-2 col">
-            <div className="h-100 card">
-              <div className="card-body">
-                <h5 className="card-title">Detalles</h5>
+
                 <div className="input-group">
-                  <label for="adults-input" className="input-group-text">
-                    Adultos
+                  <label for="children-input" className="input-group-text">
+                    Niños
                   </label>
                   <input
                     type="number"
                     min="0"
                     className="form-control"
-                    id="adults-input"
-                    aria-describedby="adults-label"
-                    onChange={(event) => setAdultos(event.target.value)}
-                    value={adultos}
+                    id="children-input"
+                    aria-describedby="children-label"
+                    onChange={(event) => setNiños(event.target.value)}
+                    value={niños}
                   />
                 </div>
-                <span id="adults-label" className="form-text">
-                  12 years old and older
+                <span id="children-label" className="form-text">
+                  2 to 12 years old
                 </span>
               </div>
-
-              <div className="input-group">
-                <label for="children-input" className="input-group-text">
-                  Niños
-                </label>
-                <input
-                  type="number"
-                  min="0"
-                  className="form-control"
-                  id="children-input"
-                  aria-describedby="children-label"
-                  onChange={(event) => setNiños(event.target.value)}
-                  value={niños}
-                />
-              </div>
-              <span id="children-label" className="form-text">
-                2 to 12 years old
-              </span>
             </div>
           </div>
         </div>
+        <button
+          type="submit"
+          id="search-button"
+          className="w-50 btn btn-primary boton-vuelos"
+        >
+          Search
+        </button>
+      </form>
+      <div className="container-md border border-dark border border-2">
+        <div>Price</div>
+        <div></div>
       </div>
-      <button id="search-button" className="w-50 btn btn-primary">
-        Search
-      </button>
-
       {/* <!-- ... --> */}
     </>
   );
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -145,17 +145,14 @@ const getState = ({ getStore, getActions, setStore }) => {
               method: "POST",
               mode: "no-cors",
               headers: {
-              
-                "Authorization": `Bearer ${store.token}`
+                Authorization: `Bearer ${store.token}`,
               },
-              body: data
-
+              body: data,
             }
-
           );
-          const data = await response.json()
-          console.log(data)
-          console.log(response)
+          const data = await response.json();
+          console.log(data);
+          console.log(response);
           // if (!response.ok) {
           //   alert("Error en el formulario, por favor verifique los datos");
           // }
@@ -183,10 +180,6 @@ const getState = ({ getStore, getActions, setStore }) => {
         }
       },
 
-
-      
-
-
       changeColor: (index, color) => {
         //get the store
         const store = getStore();
@@ -196,57 +189,110 @@ const getState = ({ getStore, getActions, setStore }) => {
         const demo = store.demo.map((elm, i) => {
           if (i === index) elm.background = color;
           return elm;
-        })},
+        });
+      },
 
-        historia: async (
-          user_id,
-          name,
-          edad,
-          peso,
-          telef,
-          correo,
-          paisRes,
-          direccion,
-          sexo,
-          alt,
-          cirugiasAnt,
-          alergias,
-          obs
-        ) => {
-          const formulario = {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              user_id: user_id,
-              name: name,
-              edad: edad,
-              peso: peso,
-              telef: telef,
-              correo: correo,
-              paisRes: paisRes,
-              direccion: direccion,
-              sexo: sexo,
-              alt: alt,
-              cirugiasAnt: cirugiasAnt,
-              alergias: alergias,
-              obs: obs,
-            }),
-          };
-          try {
-            const response = await fetch(
-              "https://3001-johndbm-proyectofinal-dfmd0gmnslt.ws-us86.gitpod.io/api/historia",
-              formulario
-            );
-            if (!response.ok) {
-              alert("Error en el formulario, por favor verifique los datos");
-            }
-            const data = await response.json;
-            console.log(data);
-          } catch (error) {
-            console.log(error);
+      historia: async (
+        user_id,
+        name,
+        edad,
+        peso,
+        telef,
+        correo,
+        paisRes,
+        direccion,
+        sexo,
+        alt,
+        cirugiasAnt,
+        alergias,
+        obs
+      ) => {
+        const formulario = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            user_id: user_id,
+            name: name,
+            edad: edad,
+            peso: peso,
+            telef: telef,
+            correo: correo,
+            paisRes: paisRes,
+            direccion: direccion,
+            sexo: sexo,
+            alt: alt,
+            cirugiasAnt: cirugiasAnt,
+            alergias: alergias,
+            obs: obs,
+          }),
+        };
+        try {
+          const response = await fetch(
+            `${proccess.env.BACKEND_URL}/api/historia`,
+            formulario
+          );
+          if (!response.ok) {
+            alert("Error en el formulario, por favor verifique los datos");
           }
-        },
+          const data = await response.json;
+          console.log(data);
+        } catch (error) {
+          console.log(error);
+        }
+      },
+
+      // flight_offers: async () => {
+      //   const vuelos = {
+      //     method: "GET",
+      //     mode: "no-cors",
+      //     headers: { "Content-Type": "application/json" ,
+      //       Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+      //     },
+      //     body: JSON.stringify({
+      //       origen: origen,
+      //       destino: destino,
+      //     }),
+      //   };
+
+      //   try {
+      //     const response = fetch(
+      //       "https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=originLocationCode&destinationLocationCode=destinationLocationCode&departureDate=departureDate&returnDate=returnDate&adults=adults",
+      //       vuelos
+      //     );
+      //     if (!response.ok) {
+      //       return False;
+      //     }
+      //     const data = await response.json;
+      //     console.log(data);
+      //   } catch (error) {
+      //     console.log(error);
+      //   }
+      // },
+
       
+
+
+
+      // flight_offers: async () => {
+      //   fetch(
+      //     "https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=MAD&destinationLocationCode=BKK&departureDate=2023-05-02&adults=1",
+      //     {
+      //       method: "GET",
+      //       mode: "no-cors",
+      //       headers: {
+      //         "Content-type": "Application/json",
+      //         Authorization: "Bearer {4u6wAEmGfJPLCChdEylj8ZTG8t1R}",
+      //       },
+      //     }
+      //   )
+      //     .then((response) => response.json())
+      //     .then((data) => {
+      //       const respuestaForm = document.getElementById("origin-input");
+      //       respuestaForm.value = JSON.stringify(data);
+      //     })
+      //     .catch((error) => console.error(error));
+      // },
+
       exampleFunction: () => {
         getActions().changeColor(0, "green");
 
